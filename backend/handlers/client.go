774586af17aaa1ea55d5830b2ccbccd886c20640
Daler <dsarifkulo021@gmail.com>
2024-11-16 17:07:34 +0500

    pagination: tested and works correctly

--- backend/handlers/client.go
+++ backend/handlers/client.go
@@ -4,6 +4,7 @@ import (
 	"backend/auth"
 	"backend/database"
 	"backend/model"
+	"backend/utils"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/google/uuid"
@@ -57,10 +58,25 @@ func CreateClient(c *fiber.Ctx) error {
 	})
 }
 
-func GetAllClients(c *fiber.Ctx) {
-	db := database.DB
+func GetAllClients(c *fiber.Ctx) error {
+	user := c.Locals("user").(*auth.Claims)
 	Clients := []model.Client{}
 
-	db.Model(&model.Client{}).Order("ID asc").Limit(100).Find(&Clients)
+	// Проверяем роль пользователя
+	var filter interface{}
+	if user.Role == "seller" {
+		// Если роль продавца, фильтруем по salesperson_id
+		filter = map[string]interface{}{"salesperson_id": user.ID}
+	}
 
+	respons, err := utils.Paginate(database.DB, c, filter, &Clients)
+
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"code":    500,
+			"message": "Failed to retrieve clients",
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(respons)
 }
