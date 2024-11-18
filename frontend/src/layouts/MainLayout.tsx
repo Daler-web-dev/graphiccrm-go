863b331dc2199cf800bb7eb46905a930a87ea5e9
MuniS97 <tursunovmunis141@gmail.com>
2024-11-18 20:26:47 +0500

    breadcrumb added

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -1,3 +1,4 @@
+import { Header } from "@/components/custom/Header";
 import SideBar from "@/components/custom/SideBar";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import React from "react";
@@ -20,8 +21,8 @@ const MainLayout: React.FC = () => {
 	return (
 		<SidebarProvider className="flex h-screen">
 			<SideBar />
-			<main className="bg-cWhite w-full min-h-screen">
-				<SidebarTrigger />
+			<main className="bg-cWhite w-full min-h-screen px-5">
+				<Header sideBarTrigger={<SidebarTrigger className="text-cDarkBlue" />} />
 				<Outlet />
 			</main>
 		</SidebarProvider>
