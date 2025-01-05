01f29f58c41beefe92fac9dbd0b603ce12380618
MuniS97 <tursunovmunis141@gmail.com>
2025-01-05 17:46:03 +0500

    -

---
+++ frontend/src/components/custom/Pagination.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+interface PaginationProps {
+    totalPages: number;
+    currentPage: number;
+    onPageChange: (page: number) => void;
+}
+
+const Pagination: React.FC<PaginationProps> = ({ totalPages, currentPage, onPageChange }) => (
+    <div className="flex justify-center mt-4 gap-2">
+        {Array.from({ length: totalPages }, (_, i) => (
+            <button
+                key={i}
+                onClick={() => onPageChange(i + 1)}
+                className={`px-3 py-1 rounded-lg ${currentPage === i + 1 ? 'bg-cOrange text-white' : 'bg-gray-200 text-gray-700'
+                    }`}
+            >
+                {i + 1}
+            </button>
+        ))}
+    </div>
+);
+
+export default Pagination;
--- frontend/src/pages/page.tsx
+++ frontend/src/pages/page.tsx
@@ -3,6 +3,36 @@ import TopRatedList from "@/components/custom/TopRatedList";
 
 
 export const Dashboard = () => {
+    // const [params, setParams] = useState();
+    // const [data, setData] = useState<Array<ISpending>>([]);
+    // const [currentPage, setCurrentPage] = useState(1);
+    // const [totalPages, setTotalPages] = useState(1);
+    // const [loading, setLoading] = useState(true);
+
+    // const loadPageData = async (page: number, queryParams?: any) => {
+    //     console.log(page, queryParams);
+
+    //     setLoading(true);
+    //     const res = await getRequest({ url: `/spending?page=${page}&limit=10`, params: queryParams });
+    //     console.log(res);
+
+    //     if (res.status === 200 || res.status === 201) {
+    //         setData(res.data.data);
+    //         setTotalPages(Math.ceil(res.data.total / 10));
+    //         setLoading(false);
+    //     } else {
+    //         toast({
+    //             title: 'Ошибка',
+    //             description: 'Произошла ошибка при загрузке расходов',
+    //             variant: 'destructive',
+    //         });
+    //     }
+    // };
+
+    // useEffect(() => {
+    //     loadPageData(currentPage, params);
+    // }, [currentPage, params]);
+
     return (
         <div className="">
             <Chart />
